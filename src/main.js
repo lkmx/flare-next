6a23aa563c3b612b64672f097bc2c30bd6148087
Luis Espinoza <luis.espinoza@lkmx.io>
2020-07-31 16:46:27 -0700

    Add first version of table component

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import SimpleHeader from './components/headers/simple-header/SimpleHeader'
 import SimpleHero from './components/heroes/simple-hero/SimpleHero'
 import SimpleNavItem from './components/navigation/simple-navigation/SimpleNavItem'
 import AnchorLink from './components/utils/anchor-link/AnchorLink'
+import SimpleTable from './components/tables/simple-table/SimpleTable'
 
 // Structures
 import NoColumn from './structures/columns/no-column/NoColumn'
@@ -23,6 +24,7 @@ const components = {
   SimpleNavItem,
   AnchorLink,
   Grid,
+  SimpleTable,
   NoColumn,
   SingleColumn,
   SlimColumn,
