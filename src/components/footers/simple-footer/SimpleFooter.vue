54bed8c2d6437ec298d703fd905dfb8fce451871
Luis Espinoza <luis.espinoza@lkmx.io>
2020-10-08 20:47:22 -0700

    Add tokens for links in footer

--- src/components/footers/simple-footer/SimpleFooter.vue
+++ src/components/footers/simple-footer/SimpleFooter.vue
@@ -4,12 +4,24 @@
   </footer>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
   .footer {
     color: $footer-text-color;
     background-color: $footer-background-color;
     min-height: $footer-min-height;
     padding: $footer-vertical-padding 0;
+    a:link {
+      color: $footer-link-default-color;
+    }
+    a:hover {
+      color: $footer-link-hover-color;
+    }
+    a:active {
+      color: $footer-link-active-color;
+    }
+    a:visited {
+      color: $footer-link-visited-color;
+    }
   }
 </style>
 
